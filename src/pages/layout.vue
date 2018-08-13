0123cae20c19a9165e08ac8d266a3f37be1d1b8c
leon <threetowns@163.com>
2018-08-13 21:17:07 +0800

    [router] Mod: config router.

---
+++ src/pages/layout.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="Layout">
+        <router-view></router-view>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const layout = r => require.ensure([], () => r(require('../pages/layout')), 'layout')
+
+const index = r => require.ensure([], () => r(require('../pages/index')), 'index')
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/', name: 'layout', component: layout,
+    redirect: '/index',
+    meta: {
+      title: 'FBC'
+    },
+    children: [
+      {
+        path: 'index', name: 'index', component: index, meta: { title: '首页' }
+      }
+    ]
+  }
+];
+
+const router = new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title ? to.meta.title : 'FBC'
+  next()
+})
+
+export default router;
