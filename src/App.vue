e4dd2897c472f9538e4c757379870674b0aaac2c
leon <threetowns@163.com>
2018-09-14 00:01:56 +0800

    [IOS] Add: window plus feature.

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,44 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    
+  },
+  created(){
+    function plusReady(){
+      let webview = plus.webview.currentWebview()
+      plus.key.addEventListener('backbutton', function() {
+        webview.canBack(function(e) {
+          if(e.canBack) {
+            webview.back();             
+          } else {
+            //处理逻辑：1秒内，连续两次按返回键，则退出应用；
+            let first = null;
+            plus.key.addEventListener('backbutton', function() {
+              //首次按键，提示‘再按一次退出应用’
+              if (!first) {
+                first = new Date().getTime();
+                plus.nativeUI.toast('再按一次退出应用');
+                setTimeout(function() {
+                    first = null;
+                }, 1000);
+              } else {
+                if (new Date().getTime() - first < 1500) {
+                  plus.runtime.quit();
+                }
+              }
+            }, false);
+          }
+        })
+      });
+    }
+    if(window.plus){
+      plusReady()
+    }else{
+      document.addEventListener("plusready", plusReady, false)
+    }
+  }
 }
 </script>
 
